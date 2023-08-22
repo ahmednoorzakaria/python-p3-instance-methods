a20463eb74769292d6e84e2a9337929eb96764b2
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-08-22 09:51:59 +0300

    Class foundation

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
 class Dog:
+  def sit(self):
+    print("The dog is sitting.")
     # Class body goes here
 
     #Instance method definition
-    pass
+
+  def bark(self):
+    print("Woof!")
+
+
+fido = Dog()
+fido.bark()
+# Woof!
+
+snoopy = Dog()
+snoopy.bark()
+# Woof!
+
